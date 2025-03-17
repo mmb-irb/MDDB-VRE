88b5205fc206eee42acfcca5291f2a98c91a9bdb
gbayarri <genis.bayarri@irbbarcelona.org>
2025-03-17 13:54:29 +0100

    Added residues and Predefined selections to NGLSelection

--- components/common/NGLSelection.vue
+++ components/common/NGLSelection.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-select
-    :label="chains.length === 0 ? 'Loading...' : (chains.length <= 1 ? `Single chain structure [:${chains[0]}]` : 'Chains')"
-    v-model="modelChains"
-    :items="chains"
-    multiple
-    :disabled="!enabled || chains.length <= 1"
-    density="compact"
-    @update:model-value="changeRepresentation"
-  ></v-select>
+  <v-row>
+    <v-col md="6" cols="12" sm="12" >
+      <v-select
+        :label="chains.length === 0 ? 'Loading...' : (chains.length <= 1 ? `Single chain structure [:${chains[0]}]` : 'Chains')"
+        v-model="modelChains"
+        :items="chains"
+        multiple
+        :disabled="!enabled || chains.length <= 1"
+        density="compact"
+        @update:model-value="changeRepresentationChains"
+      ></v-select>
+    </v-col>
+    <v-col md="6" cols="12" sm="12" >
+      <v-select
+        label="Predefined selections"
+        v-model="modelPredefSels"
+        :items="predefSels"
+        item-title="name"
+        item-value="id"
+        multiple
+        density="compact"
+        @update:model-value="changeRepresentationPdfSel"
+      >
+        <template v-slot:selection="{ item, index }">
+          <!-- Display the first two selected items -->
+          <span v-if="index < 2">
+            {{ item.title }}
+            {{ index < modelPredefSels.length - 1 && index < 1 ? ', ' : '' }}
+          </span>
+          <span
+            v-if="index === 2"
+            class="text-grey text-caption align-self-center"
+          >
+            (+{{ modelPredefSels.length - 2 }} others)
+          </span>
+        </template>
+      </v-select>
+    </v-col>
+  </v-row>  
   <v-select
     :label="residues.length === 0 ? 'Loading...' : 'Residues'"
     v-model="modelResidues"
@@ -15,7 +45,7 @@
     multiple
     :disabled="!enabled || residues.length <= 1"
     density="compact"
-    @update:model-value="changeRepresentation"
+    @update:model-value="changeRepresentationRes"
   ></v-select>
   <p class="supralabel">Custom selection. If you need help with <strong>NGL Viewer selection language</strong>, <a href="https://nglviewer.org/ngl/api/manual/usage/selection-language.html" target="_blank">please click here</a>.</p>
   <v-textarea
@@ -23,7 +53,7 @@
     label="NGL selection"
     clearable
     @update:modelValue="setSelection"
-    @click:clear="modelChains = []"
+    @click:clear="modelChains = []; modelResidues = []; modelPredefSels = []"
     :disabled="!enabled"
     rows="16"
     no-resize
@@ -33,14 +63,16 @@
 <script setup>
 
   const emit = defineEmits(['setSelection'])
-  const { $eventBus } = useNuxtApp();
+  const { $globals, $eventBus } = useNuxtApp();
 
   const modelSelection = ref('')
   const modelChains = ref([])
   const modelResidues = ref([])
+  const modelPredefSels = ref([])
   const chains = ref([])
   const residues = ref([])
   const enabled = ref(false)
+  const predefSels = $globals.ngl.predefinedSelections
 
   onMounted(() => {
     $eventBus.on('nglReady', setEnabled);
@@ -68,15 +100,35 @@
     modelSelection.value = value
     emit('setSelection', value)
 
-    // update modelChains based on the current modelSelection
     if(modelSelection.value) {
+      console.log('modelSelection.value', modelSelection.value)
+      // update modelChains based on the current modelSelection
       const currentSelectionTokens = splitSelection(modelSelection.value)
+
       // Filter out chain-specific tokens
-      const filteredSelectionTokens = currentSelectionTokens
+      const filteredSelectionTokensCh = currentSelectionTokens
       .filter(token => token.startsWith(':'))
       .map(token => token.replace(/:+/g, ''));
       // Update modelChains
-      modelChains.value = filteredSelectionTokens
+      modelChains.value = filteredSelectionTokensCh
+
+      // Filter out predef-specific tokens
+      const filteredSelectionTokensPdf = currentSelectionTokens
+      .filter(token => predefSels.some(item => item.id === token))
+      .map(token => token.replace(/:+/g, ''));
+      // Update modelChains
+      modelPredefSels.value = filteredSelectionTokensPdf
+
+      // Filter out residue-specific tokens
+      const filteredSelectionTokensRes = currentSelectionTokens
+      .filter(token => token.match(/^[\-A-Za-z0-9]{1,3}:[A-Za-z]$/))
+      // Update modelResidues
+      modelResidues.value = filteredSelectionTokensRes
+    } else {
+      // If the selection is empty, reset the modelChains, modelPredefSels, and modelResidues
+      modelChains.value = []
+      modelPredefSels.value = []
+      modelResidues.value = []
     }
   }
 
@@ -98,7 +150,7 @@
     return uniqueTokens.join(' ');
   };
 
-  const changeRepresentation = () => {
+  const changeRepresentationChains = () => {
     if (modelSelection.value && modelSelection.value.trim() !== '') {
       // Split the current selection into individual tokens
       const currentSelectionTokens = splitSelection(modelSelection.value)
@@ -113,8 +165,6 @@
         // Keep non-chain terms (e.g., 73, 50)
         return true;
       });
-
-      //console.log(filteredSelectionTokens, modelChains.value)
 
       // Add new chains from modelChains that are not already in the selection
       const newChains = modelChains.value
@@ -132,8 +182,77 @@
 
     // If modelSelection is empty, just set the selection to the current modelChains
     const chsel = modelChains.value.map(c => `:${c}`).join(' ');
+    setSelection(chsel);
+  };
+
+  const changeRepresentationPdfSel = () => {
+
+    if (modelSelection.value && modelSelection.value.trim() !== '') {
+      // Split the current selection into individual tokens
+      const currentSelectionTokens = splitSelection(modelSelection.value)
+
+      // Filter out predefined-specific tokens that are no longer in modelPredefSels
+      const filteredSelectionTokens = currentSelectionTokens.filter(token => {
+        // Check if the token is a predefined-specific term (e.g., helix, sheet)
+        if (predefSels.some(item => item.id === token)) {
+          return modelPredefSels.value.includes(token); // Keep only if the predefined selection is still in modelPredefSels
+        }
+        // Keep non-predefined terms (e.g., :A, 1:45)
+        return true;
+      });
+
+      // Add new predefined selections from modelPredefSels that are not already in the selection
+      const newSelections = modelPredefSels.value
+        .filter(selection => !currentSelectionTokens.includes(selection))
+        .map(selection => `${selection}`);
+
+      // Combine the filtered selection and new predefined selections
+      const combinedSelection = [...filteredSelectionTokens, ...newSelections].join(' ');
+
+      // Remove any repeated terms and set the new selection
+      const newSel = removeRepeatedInSelection(combinedSelection);
+      setSelection(newSel);
+      return;
+    }
+
+    // If modelSelection is empty, just set the selection to the current modelPredefSels
+    const resel = modelPredefSels.value.map(c => `${c}`).join(' ');
+    setSelection(resel);
+  };
+
+  const changeRepresentationRes = () => {
+
+    if (modelSelection.value && modelSelection.value.trim() !== '') {
+      // Split the current selection into individual tokens
+      const currentSelectionTokens = splitSelection(modelSelection.value)
+
+      // Filter out residue-specific tokens that are no longer in modelResidues
+      const filteredSelectionTokens = currentSelectionTokens.filter(token => {
+        // Check if the token is a residue-specific term (e.g., 1:J, -73:C)
+        if (token.match(/^[\-A-Za-z0-9]{1,3}:[A-Za-z]$/)) {
+          return modelResidues.value.includes(token); // Keep only if the residue is still in modelResidues
+        }
+        // Keep non-residue terms (e.g., :A, sheet)
+        return true;
+      });
+
+      // Add new residues from modelResidues that are not already in the selection
+      const newResidues = modelResidues.value
+        .filter(residue => !currentSelectionTokens.includes(residue))
+        .map(residue => `${residue}`);
+
+      // Combine the filtered selection and new residues
+      const combinedSelection = [...filteredSelectionTokens, ...newResidues].join(' ');
+
+      // Remove any repeated terms and set the new selection
+      const newSel = removeRepeatedInSelection(combinedSelection);
+      setSelection(newSel);
+      return;
+    }
+
+    // If modelSelection is empty, just set the selection to the current modelResidues
     const resel = modelResidues.value.map(c => `${c}`).join(' ');
-    setSelection(chsel + ' ' + resel);
+    setSelection(resel);
   };
 
   defineExpose({
@@ -146,4 +265,12 @@
 
 <style scoped>
   .supralabel { font-size: 13px; }
+  .truncate {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
+    /*max-width: v-bind(truncateWidth);  */
+    max-width: 100%;
+  }
 </style>
